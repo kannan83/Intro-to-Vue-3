79c07a6bf39a7f93800eedffe579f17ebb67282e
kannan83 <karoumont.travail@gmail.com>
2021-03-22 16:06:45 +0100

    L2-end

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 const product = 'Socks'
+
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            description: 'this is the description '
+        }
+    }
+})
+
